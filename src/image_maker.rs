b88c36e4433595f26c0b5c394a0349482a79ffde
gwy15 <gwy15thu@gmail.com>
2023-01-05 20:30:14 +0800

    draw text with better shadow

--- src/image_maker.rs
+++ src/image_maker.rs
@@ -115,20 +115,22 @@ fn system_open(path: &Path) -> Result<()> {
 }
 
 fn draw_text(img: &mut Mat, text: &str, x: u32, y: u32, im_h: u32) -> Result<()> {
-    // render text
-    let point = cv_core::Point::new((x + 5) as i32, (y + im_h - 5) as i32);
-    // let point = cv_core::Point::new(pos.x, pos.y);
-    imgproc::put_text(
-        img,
-        text,
-        point,
-        imgproc::FONT_HERSHEY_DUPLEX,
-        0.9,
-        cv_core::Scalar::all(255.),
-        1,
-        imgproc::LINE_8,
-        false,
-    )?;
+    const DATA: &[(u32, f64)] = &[(2, 16.), (1, 0.), (0, 255.)];
 
+    // 先写一个黑色的背景
+    for (offset, color) in DATA {
+        let point = cv_core::Point::new((x + 5 + offset) as i32, (y + im_h - 5 + offset) as i32);
+        imgproc::put_text(
+            img,
+            text,
+            point,
+            imgproc::FONT_HERSHEY_DUPLEX,
+            0.9,
+            cv_core::Scalar::all(*color),
+            1,
+            imgproc::LINE_8,
+            false,
+        )?;
+    }
     Ok(())
 }
