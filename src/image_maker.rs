3c8e02b57128b436cd77cc752f09c7ae83e8b5fa
gwy15 <gwy15thu@gmail.com>
2023-01-07 17:27:51 +0800

    update to opencv 0.74

--- src/image_maker.rs
+++ src/image_maker.rs
@@ -24,7 +24,7 @@ pub fn open_frame_data(
         let row_data = &data[row_start..row_end];
 
         unsafe {
-            let mat_ptr: &mut u8 = mat.ptr_mut(row as i32)?;
+            let mat_ptr: &mut u8 = &mut *(mat.ptr_mut(row as i32)?);
             let mat_slice: &mut [u8] = std::slice::from_raw_parts_mut(mat_ptr, row_data.len());
             mat_slice.copy_from_slice(row_data);
         }
